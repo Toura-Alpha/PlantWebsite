219db8038d13784cebb5e92b69af8a444d3841d2
Toura-Alpha <steronenombiekenne@gmail.com>
2025-08-12 11:31:14 +0100

    set up for tailwindcss completed

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.{html,js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
